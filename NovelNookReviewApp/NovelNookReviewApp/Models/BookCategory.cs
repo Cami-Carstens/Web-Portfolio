c867dcab51c1980ed981212c7c5d9d5a530cb871
Cami-Carstens <camisuec@aol.com>
2026-08-14 12:28:38 -0500

    New App work and resume

---
+++ NovelNookReviewApp/NovelNookReviewApp/Models/BookCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace NovelNookReviewApp.Models
+{
+    public class BookCategory
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public Book Book { get; set; }
+
+        public Category Category { get; set; }
+    }
+
+}
---
+++ NovelNookReviewApp/NovelNookReviewApp/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace NovelNookReviewApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<BookCategory> BookCategories { get; set; }
+    }
+}
